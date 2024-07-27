50b6f13d21d5a75797e67b0be3318acb40fb0baa
Srivatsan <sriram.srini12@gmail.com>
2024-07-27 14:57:22 +0530

    created home page

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
+import { Results, ResultsSkeleton } from "./_components/results";
+
 export default function Home() {
   return (
-    <main>
-      <p>Home Page</p>
-    </main>
+    <div className="h-full p-8 max-w-screen-2xl mx-auto">
+      <Suspense fallback={<ResultsSkeleton />}>
+        <Results />
+      </Suspense>
+    </div>
   );
 }
